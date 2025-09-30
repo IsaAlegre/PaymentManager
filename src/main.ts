7947048273f678ee7bd4f4d9de8a0e83b8764663
IsaAlegre <isabellaalegre84@gmail.com>
2025-09-30 01:06:19 -0300

    refactor(app): Migrate to standalone bootstrap and routing

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import {
-  provideHttpClient,
-  withFetch,
-  withInterceptors,
-} from '@angular/common/http';
-import {
-  importProvidersFrom,
-  provideZonelessChangeDetection,
-} from '@angular/core';
-import { AppRoutingModule } from './app/app-routing-module';
-import { authInterceptor } from './app/core/interceptors/auth.interceptor';
+import { appConfig } from './app/app.config'; // <-- Solo importamos la configuración
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideZonelessChangeDetection(),
-    importProvidersFrom(AppRoutingModule),
-    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
