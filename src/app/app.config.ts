af259e430fe3f7445524fcdb043c4b9e0397c35e
IsaAlegre <isabellaalegre84@gmail.com>
2025-09-30 15:49:14 -0300

    update settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZonelessChangeDetection} from '@angular/core'
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
-import { routes } from './app.routes'; // Lo crearemos en el siguiente paso
+import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,17 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  // Redirige la ruta raíz ('') a '/payments'
   {
     path: '',
     redirectTo: '/payments',
     pathMatch: 'full',
   },
-  // Carga las rutas de la feature 'payment-requests'
+  
   {
     path: 'payments',
     loadChildren: () =>
       import('./features/payment-requests/payment-requests.module').then(
-        (m) => m.PaymentRequestsModule // Asumiendo que tus rutas de feature también son standalone
+        (m) => m.PaymentRequestsModule 
       ),
   },
 ];
